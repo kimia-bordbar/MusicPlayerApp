3010dbb0378b9d7e70853a10a5da1cdbafa10b57
kimia-bordbar <bordbar.pro@yahoo.com>
2022-06-22 23:56:23 +0430

    initial commit

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=P:\flutter_development_SDK\flutter"
+export "FLUTTER_APPLICATION_PATH=P:\Projects\flutter\music_player\music_player"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "P:\\flutter_development_SDK\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "P:\\Projects\\flutter\\music_player\\music_player" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=P:\\flutter_development_SDK\\flutter"
+  "PROJECT_DIR=P:\\Projects\\flutter\\music_player\\music_player"
+  "FLUTTER_ROOT=P:\\flutter_development_SDK\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=P:\\Projects\\flutter\\music_player\\music_player\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=P:\\Projects\\flutter\\music_player\\music_player"
+  "FLUTTER_TARGET=P:\\Projects\\flutter\\music_player\\music_player\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=P:\\Projects\\flutter\\music_player\\music_player\\.dart_tool\\package_config.json"
+)
